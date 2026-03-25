a92aa8520a8c6a38d0ef8e34e023c610070b7957
ferdinandroot <ferdinandjosealicia@gmail.com>
2026-03-25 15:57:39 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'dhpp-campo-v1';
+const ASSETS = ['./index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const network = fetch(e.request).then(resp => {
+        if (resp.ok) caches.open(CACHE).then(c => c.put(e.request, resp.clone()));
+        return resp;
+      }).catch(() => cached);
+      return cached || network;
+    })
+  );
+});
